f2b0bcd9d937ef1a1e9e44b6270ccdb6afbe07a3
Juani Schuhmann <76596375+jschuhmann47@users.noreply.github.com>
2024-10-05 15:16:40 -0300

    Feature/area with one parent (#29)
    * add tests and code for getting parent
    
    * create interface with library
    
    * refactor whole diagram classW
    
    * change method to function since it's not working
    
    * regen package lock
    
    * restore old package lock
    
    * rm comments
    
    * validate

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -13,7 +13,7 @@ import { Project } from './project.schema'
 import { defaultStages, Permission, Stage, StageType } from './stage.schema'
 import { insensitiveRegExp } from './utils/escape_string'
 import { User } from 'src/user/user.schema'
-import { OrganizationChart } from './orgChart'
+import { getParentsFromNode, OrganizationChart } from './orgChart'
 import { OkrService } from 'src/herramientas/okr/okr.service'
 
 @Injectable()
@@ -153,6 +153,14 @@ export class ProjectService {
     }
 
     async addChart(projectId: string, chart: OrganizationChart) {
+        chart.nodes.forEach((node) => {
+            if (getParentsFromNode(node.id, chart).length > 1) {
+                throw new HttpException(
+                    'Diagrama tiene algún área con más de un área padre',
+                    HttpStatus.BAD_REQUEST
+                )
+            }
+        })
         const project = await this.projectModel.findById(projectId)
         if (project.chart) {
             this.updateMissingAreas(project, chart)
