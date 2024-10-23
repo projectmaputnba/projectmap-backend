68fd01d0e0ce524ca560a8eea25f895a5ab2748d
T-J-Antonio <tantonio@frba.utn.edu.ar>
2024-10-23 19:14:58 -0300

    hotfix: exclude pestel/options from middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -53,6 +53,7 @@ export class AppModule {
         consumer
             .apply(ProjectStageUserEditionMiddleware)
             .exclude('questionnaires/questions')
+            .exclude('pestel/options')
             .forRoutes(
                 'foda',
                 'porter',
