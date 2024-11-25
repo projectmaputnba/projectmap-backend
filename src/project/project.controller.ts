254b8b1e075b3e82f65e75e63cc809a920edf0f5
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2024-11-24 21:32:54 -0300

    readd missing endponts

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -3,6 +3,8 @@ import {
     Controller,
     Delete,
     Get,
+    HttpException,
+    HttpStatus,
     ImATeapotException,
     Param,
     Post,
@@ -13,20 +15,42 @@ import {
 } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import {
-    ApiTags,
     ApiOperation,
-    ApiResponse,
     ApiParam,
     ApiQuery,
+    ApiResponse,
+    ApiTags,
 } from '@nestjs/swagger'
-import { ProjectDto } from './project.dto'
+import { PdcaService } from 'src/herramientas/pdca/pdca.service'
+import { AnsoffService } from '../herramientas/ansoff/ansoff.service'
+import { BalancedScorecardService } from '../herramientas/balancedScorecard/balancedScorecard.service'
+import { FodaService } from '../herramientas/foda/foda.service'
+import { MckinseyService } from '../herramientas/mckinsey/mckinsey.service'
+import { OkrService } from '../herramientas/okr/okr.service'
+import { PestelService } from '../herramientas/pestel/pestel.service'
+import { PorterService } from '../herramientas/porter/porter.service'
+import { QuestionnaireService } from '../herramientas/questionnaire/questionnaire.service'
+import { OrganizationChart } from './orgChart'
+import { ProjectDto, UpdateUserRolesDto } from './project.dto'
 import { ProjectService } from './project.service'
+import { isValidStageType, isValidPermission, StageType } from './stage.schema'
 
 @UseGuards(AuthGuard('jwt'))
 @ApiTags('projects')
 @Controller('projects')
 export class ProjectController {
-    constructor(private projectService: ProjectService) {}
+    constructor(
+        private projectService: ProjectService,
+        private fodaService: FodaService,
+        private pestelService: PestelService,
+        private ansoffService: AnsoffService,
+        private porterService: PorterService,
+        private mckinseyService: MckinseyService,
+        private okrService: OkrService,
+        private balancedService: BalancedScorecardService,
+        private questionnaireService: QuestionnaireService,
+        private pdcaService: PdcaService
+    ) {}
 
     @Get('/shared')
     @ApiOperation({ summary: 'Throws an HTTP 418 error' })
@@ -72,6 +96,60 @@ export class ProjectController {
             items: projects,
             total,
         }
+    }
+
+    @Get(':id/foda')
+    async getFoda(@Param('id') id: string) {
+        const fodas = await this.fodaService.getAllByProjectId(id)
+        return fodas
+    }
+    @Get(':id/pestel')
+    async getPestel(@Param('id') id: string) {
+        const pestels = await this.pestelService.getAllByProjectId(id)
+        return pestels
+    }
+    @Get(':id/ansoff')
+    async getAnsoff(@Param('id') id: string) {
+        const ansoffs = await this.ansoffService.getAllByProjectId(id)
+        return ansoffs
+    }
+    @Get(':projectId/porter')
+    async getPorter(@Param('projectId') projectId: string) {
+        const porters = await this.porterService.getAllByProjectId(projectId)
+        return porters
+    }
+    @Get(':projectId/mckinsey')
+    async getMcKinsey(@Param('projectId') projectId: string) {
+        const mckinseys =
+            await this.mckinseyService.getAllByProjectId(projectId)
+        return mckinseys
+    }
+    @Get(':projectId/okr-projects')
+    async getOkr(@Param('projectId') projectId: string) {
+        return this.okrService.getAllByProjectId(projectId)
+    }
+    @Get(':projectId/balanced-scorecards')
+    async getBalancedScorecards(@Param('projectId') projectId: string) {
+        return this.balancedService.getAllByProjectId(projectId)
+    }
+    @Get(':projectId/questionnaires')
+    async getQuestionnaires(@Param('projectId') projectId: string) {
+        return this.questionnaireService.findByProjectId(projectId)
+    }
+    @Get(':projectId/pdcas')
+    async getPdcas(@Param('projectId') projectId: string) {
+        return this.pdcaService.findByProjectId(projectId)
+    }
+    @Post(':id/organizational-chart')
+    async saveOrganizationalChart(
+        @Param('id') projectId: string,
+        @Body() chart: OrganizationChart
+    ) {
+        return this.projectService.addChart(projectId, chart)
+    }
+    @Get(':id/organizational-chart')
+    async getOrganizationalChart(@Param('id') projectId: string) {
+        return this.projectService.getChart(projectId)
     }
 
     @Get(':id')
@@ -128,5 +206,79 @@ export class ProjectController {
         return {
             _id: projectId,
         }
+    }
+
+    @Put(':id/roles')
+    async updateUserRoles(
+        @Req() header: { user: { id: string } },
+        @Param('id') projectId: string,
+        @Body() req: UpdateUserRolesDto
+    ) {
+        if (!req || !req.users) {
+            throw new HttpException(
+                'Missing users to update',
+                HttpStatus.BAD_REQUEST
+            )
+        }
+        req.users.forEach((user) => {
+            if (!user.role || !user.userId) {
+                throw new HttpException(
+                    'Invalid fields',
+                    HttpStatus.BAD_REQUEST
+                )
+            }
+            if (user.role != 'coordinator' && user.role != 'participant') {
+                throw new HttpException('Invalid role', HttpStatus.BAD_REQUEST)
+            }
+            if (user.role == 'participant' && !Array.isArray(user.stages)) {
+                throw new HttpException(
+                    'Invalid fields',
+                    HttpStatus.BAD_REQUEST
+                )
+            }
+            if (user.role == 'participant') {
+                user.stages.forEach((s) => {
+                    if (
+                        !isValidStageType(s.id) ||
+                        !isValidPermission(s.permission)
+                    )
+                        throw new HttpException(
+                            'Invalid stage or permission',
+                            HttpStatus.BAD_REQUEST
+                        )
+                })
+                if (user.stages.length != Object.keys(StageType).length) {
+                    throw new HttpException(
+                        'Invalid stage count',
+                        HttpStatus.BAD_REQUEST
+                    )
+                }
+            }
+        })
+        const project = await this.projectService.updateUserRoles(
+            header.user.id,
+            projectId,
+            req
+        )
+        return project
+    }
+    @Post(':id/user/add')
+    async addUserToProject(
+        @Req() header: { user: { id: string } },
+        @Param('id') projectId: string,
+        @Body()
+        req: {
+            userEmail: string
+            role: string
+        }
+    ) {
+        const { id } = header.user
+        const project = await this.projectService.addUserToProject(
+            projectId,
+            req.userEmail,
+            req.role,
+            id
+        )
+        return project
     }
 }
